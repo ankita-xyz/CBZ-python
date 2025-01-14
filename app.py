2d82bc9dc58f50b1a3205ed1b2c39a0a36be1719
Pratik Ramteke <148608558+PratikPhysics@users.noreply.github.com>
2025-01-14 15:18:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+x = 3
+y = 2
+#print(x/y) #int? float?
+
+# print(11%3)
+
+# print('-------')
+# print(x//y)
+
+value = 10*(5 + 2 - 10)/2 + True
+#BODMAS rule!!
+
+
+print(True + True*5 + False) # -----> 6
---
+++ test.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    if i == 7:
+        break  # Exit the loop immediately
+    print("Iteration:", i)
+
+
+
+
